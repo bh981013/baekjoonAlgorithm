fa7b4c85a48c537df06561d2d05457b12347acbc
bh981013 <84774907+bh981013@users.noreply.github.com>
2022-02-23 23:54:05 +0900

    Create contact.py

---
+++ contact.py
@@ -0,0 +1,26 @@
+import sys
+import heapq
+myinput = sys.stdin.readline
+# sys.setrecursionlimit(1000000)
+import time
+import re
+start = 0
+def main():i
+    T =  int(input())
+    signals = []
+    output = []
+    for _ in range(T):
+        signals.append(input().strip())
+    p = re.compile("(100+1+|01)+")
+    for signal in signals:
+        m = p.fullmatch(signal)
+        if m!= None and m.span() == (0, len(signal)):
+            output.append("YES")
+        else: output.append("NO")
+        
+    print("\n".join(output))
+
+
+
+    
+main()
